b6202d5feceb7b92f2cdddb2e6bfb10dd7e4f56c
Estudiante <iluna@vallegrande.edu.pe>
2018-05-26 10:39:08 -0500

    Terminamos el script sobre el manejo de BD en ms server

--- lab01/QueryOne.sql
+++ lab01/QueryOne.sql
@@ -3,6 +3,34 @@ go
 
 select database_id, name 
 from sys.databases
-
+/*ver listado de AdventureVG_copia*/
 use AdventureVG_copia
 select name from sys.tables;
+/*ver listado de vistas de una base de datos */
+use AdventureVG_copia
+select name from SYS.views ;
+GO
+/*visualizar la informacion detallada de una base de datos */
+exec sp_helpdb AdventureVG_copia;
+
+/*utilizando snippets*/
+-- Create a new database called 'MercadoVG'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'MercadoVG'
+)
+CREATE DATABASE MercadoVG
+GO
+
+/*eliminar una base de datos*/
+drop database HospitalHHH;
+GO
+
+/*Eliminar base de datos AdventureVG_copia*/
+drop database AdventureVG_copia;
+go
