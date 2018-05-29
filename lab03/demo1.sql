146d0988edfd2b711ab17f61347d57e02eb365bc
Estudiante <iluna@vallegrande.edu.pe>
2018-05-29 11:55:27 -0500

    laboratorio 3

--- lab03/demo1.sql
+++ lab03/demo1.sql
@@ -1,1 +1,27 @@
-fefdsf
+use AdventureWorks2016
+
+select *from Person.Person
+
+create VIEW vw_Persona
+as select firstName,LastName
+from Person.Person
+
+select*from vw_Persona
+
+
+create VIEW vw_Persona2
+as select firstName,LastName
+from Person.Person
+where LastName like 'Adams'
+
+drop view vw_Persona2
+
+select*from vw_Persona2
+
+create VIEW vw_Persona3 
+AS
+select CONCAT(firstName,',',LastName) as datos
+from Person.Person
+Where LastName LIKE 'Adams'
+select*from vw_Persona3
+
