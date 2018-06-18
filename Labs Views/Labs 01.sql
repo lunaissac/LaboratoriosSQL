c157d3189dfb3b3c0d9cb5fa874d5b612a73c0cd
lunaissac <31965093+lunaissac@users.noreply.github.com>
2018-06-18 11:46:30 -0500

    Add files via upload
    super

---
+++ Labs Views/Labs 01.sql
@@ -0,0 +1,12 @@
+/* poner en uso la base de datos*/
+
+use AdventureWorks2016;
+
+--muestrame el nombre, apellido, departamento, estado civil del empleado ---
+
+
+---muestrame el listado de clientes que hicieron su pedido de compra en los meses de noviembre y diciembre del año 2012 ---
+
+
+
+---Muestrame la cantidad de ordenes de pedido atendidos por cada medio de envio ---
